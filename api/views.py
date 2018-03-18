21519e821d0f5780c1371caf7892d7f003477414
Deeptanshu Malik <deeptanshumalik@gmail.com>
2018-03-18 04:03:26 -0400

    added test to see if user is allowed to see the view

--- api/views.py
+++ api/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -9,17 +11,23 @@ import shutil
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 def send_zip(request):
-    """
-    List all code snippets, or create a new snippet.
-    """
+
+
     if request.method == 'GET':
-        shutil.make_archive("files", 'zip', BASE_DIR + "/media/")
-        file_path = BASE_DIR + "/files.zip"
-        filename = "files.zip"
-        # Grab ZIP file from in-memory, make response with correct MIME-type
-        response = HttpResponse(open(file_path, 'rb').read(),\
-                                                         content_type='application/zip')
-        response['Content-Disposition'] = 'attachment; filename=filename'
+        if request.user.username == "THE_ONE_ALLOWED_USER":
+            shutil.make_archive("files", 'zip', BASE_DIR + "/media/")
+            file_path = BASE_DIR + "/files.zip"
+            filename = "files.zip"
+            # Grab ZIP file from in-memory, make response with correct MIME-type
+            response = HttpResponse(open(file_path, 'rb').read(),\
+                                                             content_type='application/zip')
+            response['Content-Disposition'] = 'attachment; filename=filename'
+            return response
+
+        # otherwise ask for authentification
+        response = HttpResponse("")
+        response.status_code = 401
+        response['WWW-Authenticate'] = 'Basic realm="restricted area"'
         return response
+
