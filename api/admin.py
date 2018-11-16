6c1cbdb161e26dc60bc21a0343365e67081f1044
Anirudh Vegesana <anirudh.vegesana@gmail.com>
2018-11-16 12:08:12 -0500

    Local deploy enabled
    To setup local deploy for this site, run the following script:
    
    git clone https://github.com/ieeelpirc/LPIRC_2018_Track1_Submission/ lpirc
    cd lpirc
    mkdir submissions_track2/ submissions_track1/ model_validation/ upload/ upload2/
    echo>local_settings.py
    manage.py migrate
    manage.py createsuperuser
    
    It will setup the site so it can be run on a local machine.

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Score)
+admin.site.register(Score_r2)
+admin.site.register(Score_r2_detection)
